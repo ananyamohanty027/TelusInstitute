147a81bcddc70b50c0dd8d49055fda5297c9a518
Ananya Mohanty <125736611+ananyamohanty027@users.noreply.github.com>
2026-01-12 23:54:10 +0530

    Delete backend/routes directory

--- backend/routes/certificateRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateCertificate, getCertificate } = require('../controllers/certificateController');
-
-router.post('/', generateCertificate);
-router.get('/:studentId', getCertificate);
-
-module.exports = router; 
